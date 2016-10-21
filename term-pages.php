9077c979dccf7d12d7079b3a742b75dee584f30c
Katharina <katharina@catmacpro.fritz.box>
2016-10-21 11:28:43 +0200

    added term-pages

--- term-pages.php
+++ term-pages.php
@@ -15,7 +15,57 @@ class Term_Pages {
 	 * Class construct method. Adds actions to their respective WordPress hooks.
 	 */
 	public function __construct() {
+		
+	$args = array(
+	'public'   => true,
+	'_builtin' => true,
+ 	); 
+ 	$output = 'names';  	
+ 	$operator = 'and'; 	
+	$taxonomies = get_taxonomies( $args, $output, $operator );
+	
+	foreach( $taxonomies as $taxonomy) {
+		
+		  echo '<h1> hallo taxo: ' . $taxonomy . '</h1>';
+	
+		  add_action( $taxonomy.'_add_form_fields', array($this,'add_extra_taxonomy_field' ));
+		  add_action( $taxonomy.'_edit_form_fields',array($this, 'edit_extra_taxonomy_field'),10,2);
+		  add_action( 'created_'.$taxonomy, array($this,'save_extra_field' ));
+		  add_action( 'edited_'.$taxonomy, array($this, 'update_extra_field' ),10,2);
+		}
+	}
+	
+function add_extra_taxonomy_field( $taxonomy ) { 
+	global $orpageid;
+ ?><div class="form-field term-group">
+    <label for="featuret-group"><?php _e('overriding page', 'term-pages'); ?></label>
+	<input type="text" id="or-page-id" name="or-page-id" size="20" value="<?php echo esc_attr( $orpageid); ?>">
+    </div><?php
+} 
 
+function save_extra_field( $term_id, $tt_id ){
+	if( isset( $_POST['or-page-id'] ) && '' !== $_POST['or-page-id'] ){
+		$group = sanitize_title( $_POST['or-page-id'] );
+		add_term_meta( $term_id, 'or-page-id', $group, true );
 	}
 }
+
+function edit_extra_taxonomy_field( $term, $taxonomy ){
+	$orpageid = get_term_meta( $term->term_id, 'or-page-id', true );
+    
+	?><tr class="form-field term-group-wrap">
+	<th scope="row"><label for="feature-group"><?php _e( 'overriding page id', 'term-pages' ); ?></label></th>
+	<td><input type="text" id="or-page-id" name="or-page-id" size="20" value="<?php echo esc_attr( $orpageid); ?>">
+	</tr><?php
+}
+
+function update_extra_field( $term_id, $tt_id){
+	if( isset( $_POST['or-page-id'] ) && '' !== $_POST['or-page-id'] ){
+	$group = sanitize_title( $_POST['or-page-id'] );
+	update_term_meta( $term_id, 'or-page-id', $group );
+	}
+}
+   
+}
+
 new Term_Pages;
