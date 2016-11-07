baba11210b5b767eabc225fc872f0ca6ab7c213e
Katharina <katharina@catmacpro.fritz.box>
2016-11-07 09:43:55 +0100

    autocomplete

--- term-pages.php
+++ term-pages.php
@@ -17,7 +17,13 @@ class Term_Pages {
 	public function __construct() {
 		add_action( 'init', array($this, 'add_taxonomie_fields'), 10);
  		add_action( 'pre_get_posts', array( $this, 'custom_page_query' ),1);	
-//		add_action( 'template_redirect', array( $this, 'hooks_setup' ), 20 );	
+//		add_action( 'template_redirect', array( $this, 'hooks_setup' ), 20 );
+		add_action('admin_enqueue_scripts', array($this, 'se_admin_enqueue_scripts'),10);
+//		add_action('wp_head', array($this, 'se_wp_head'),10);
+		add_action('wp_ajax_se_lookup',array($this, 'se_lookup'),8);
+		add_action('wp_ajax_nopriv_se_lookup',array($this, 'se_lookup'),8);
+		add_action('admin_footer',array($this, 'my_admin_function'),9);
+		
 	}
 
 	function add_taxonomie_fields(){
@@ -32,13 +38,52 @@ class Term_Pages {
 			
 			}
 	}
-	
+
+function se_admin_enqueue_scripts() {
+    wp_enqueue_script('suggest');
+    wp_enqueue_style( 'suggest' );
+}
+
+function my_admin_function() {
+?>
+<script type="text/javascript">
+  jQuery(document).ready(function() {
+    
+    
+ 
+
+   var se_ajax_url = '<?php echo admin_url('admin-ajax.php'); ?>';
+   jQuery('.or-page-id').suggest(se_ajax_url + '?action=se_lookup');
+   
+   
+   
+
+/*
+   var myString = jQuery("input[name=or-page-id]").val(); 
+   console.log(myString);
+   var myNewString = myString.split("-");
+   console.log(myNewString[1]);
+   jQuery("input[name=or-page-id]").focusout("input[name=or-page-id]").val(myNewString[1]); 
+*/
+
+
+  });
+</script>
+<?php
+}
+
+
 function add_extra_taxonomy_field( $taxonomy ) { 
 	global $orpageid;
  ?><div class="form-field term-group">
-    <label for="featuret-group"><?php _e('overriding page', 'term-pages'); ?></label>
-	<input type="text" id="or-page-id" name="or-page-id" size="20" value="<?php echo esc_attr( $orpageid); ?>">
-    </div><?php
+    <label for="feature-group"><?php _e('overriding page', 'term-pages'); ?></label>
+	<input type="text" class="or-page-id" name="or-page-id" size="20" value="<?php echo esc_attr( $orpageid); ?>">
+	
+    </div>
+
+    
+    
+    <?php
 } 
 
 function save_extra_field( $term_id, $tt_id ){
@@ -49,23 +94,63 @@ function save_extra_field( $term_id, $tt_id ){
 }
 
 function edit_extra_taxonomy_field( $term, $taxonomy ){
-	$orpageid = get_term_meta( $term->term_id, 'or-page-id', true );
+	$post = get_term_meta( $term->term_id, 'or-page-id', true );
+	
+	$orpageid = get_the_title( $post );
+	$orpageid .= ':'.$post;
     
 	?><tr class="form-field term-group-wrap">
 	<th scope="row"><label for="feature-group"><?php _e( 'overriding page id', 'term-pages' ); ?></label></th>
-	<td><input type="text" id="or-page-id" name="or-page-id" size="20" value="<?php echo esc_attr( $orpageid); ?>">
+	
+	
+	<td><input type="text" class="or-page-id" name="or-page-id" size="20" value="<?php echo esc_attr( $orpageid); ?>">
 	</tr><?php
 }
 
 function update_extra_field( $term_id, $tt_id){
 	if( isset( $_POST['or-page-id'] ) && '' !== $_POST['or-page-id'] ){
-	$group = sanitize_title( $_POST['or-page-id'] );
+	//$str = sanitize_title( $_POST['or-page-id'] );
+	$str = $_POST['or-page-id'];
+	 
+
+ 
+  preg_match('/(?<name>\w+) : (?<zahl>\d+)/', $str, $treffer);
+		
+	$group = $treffer[2];
+	
 	update_term_meta( $term_id, 'or-page-id', $group );
 	}
 	else if ( $_POST['or-page-id'] == ''){
 	delete_term_meta( $term_id, 'or-page-id');	
 		
 	}
+}
+
+
+
+
+function se_lookup() {
+    global $wpdb;
+
+    $search = like_escape($_REQUEST['q']);
+
+  $query = 'SELECT ID,post_title FROM ' . $wpdb->posts . '
+        WHERE post_title LIKE \'' . $search . '%\'
+        AND post_type = \'page\'
+        AND post_status = \'publish\'
+        ORDER BY post_title ASC';
+        
+    foreach ($wpdb->get_results($query) as $row) {
+        $post_title = $row->post_title;
+        $id = $row->ID;
+
+       
+
+        echo $post_title . ' : ' . $id. "\n";
+    }
+    
+    
+    die();
 }
 
 
